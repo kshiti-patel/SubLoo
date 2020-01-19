cd70441743fab13378bfc1ca93bfc762ab1ed8ff
kshiti-patel <47679159+kshiti-patel@users.noreply.github.com>
2020-01-19 13:48:22 -0500

    Add files via upload

---
+++ Models/Listing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SubLoo.Models
+{
+    public class Listing
+    {
+        public string Location { get; set; }
+        public string Gender  { get; set; }
+        public string Duration { get; set; }
+        public int Price { get; set; }
+    }
+}
